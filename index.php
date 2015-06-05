d5a01721dfc128e80dceab68b3c4f2c41c5c9295
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-06-05 13:47:10 +0900

    initial-commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require "vendor/autoload.php";
+
+use \Underscore\Types\Arrays;
+use \Underscore\Types\Object;
+use \Underscore\Types\Number;
+use \Underscore\Types\String;
+use \Underscore\Types\Functions;
+use \Underscore\Types\Parse;
